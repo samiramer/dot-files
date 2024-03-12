f1d8ed071246543484d04e64cdfa02d0ae641612
Samir Amer <samir@sunnybyte.com>
2024-03-12 16:43:33 -0400

    feat: change wallpaper image

--- xinit/.config/wallpaper/blur.sh
+++ xinit/.config/wallpaper/blur.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 # Add a lock icon and text to the center of an image
-convert wall.png -font Noto-Sans \
+convert wall.png -font Noto-Sans-Regular \
     -blur 0x25 \
     -pointsize 26 -fill white -gravity center \
     -annotate +0+240 "Type Password to Unlock" lock.png \
