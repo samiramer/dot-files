56dd91f1ade2f42aae0db57af0fe434a9e64b74d
samer <samirmamer@gmail.com>
2025-12-11 20:25:30 -0500

    feat: add startx configuration and supplimentary scripts

---
+++ startx/.config/wallpaper/blur.sh
@@ -0,0 +1,7 @@
+#!/bin/sh
+# Add a lock icon and text to the center of an image
+convert wall.png -font Noto-Sans \
+    -blur 0x25 \
+    -pointsize 26 -fill white -gravity center \
+    -annotate +0+240 "Type Password to Unlock" lock.png \
+    -gravity center -composite blurred-wall.png
